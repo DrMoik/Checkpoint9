a2fba37d0858a457837bf5a2712673b6f2fd4227
Miguel GZ <m.galvez@tec.mx>
2023-12-30 07:05:31 +0000

    All ok

--- eight_trajectory/src/eight_trajectory.cpp
+++ eight_trajectory/src/eight_trajectory.cpp
@@ -1,128 +1,85 @@
-#include <array>
-#include <cmath>
 #include <nav_msgs/msg/odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/float32_multi_array.hpp>
-#include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
-#include <vector>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tuple>
 
-class EightTrajectoryNode : public rclcpp::Node {
-public:
-  EightTrajectoryNode() : Node("eight_trajectory") {
-    odom_subscriber_ = this->create_subscription<nav_msgs::msg::Odometry>(
-        "/odometry/filtered", 10,
-        std::bind(&EightTrajectoryNode::odomCallback, this,
-                  std::placeholders::_1));
-    wheel_speed_publisher_ =
-        this->create_publisher<std_msgs::msg::Float32MultiArray>("/wheel_speed",
-                                                                 10);
+double phi; // Initialize phi with an appropriate initial value
 
-    waypoints_ = {{{0.0, 1, -1},
-                   {0.0, 1, 1},
-                   {0.0, 1, 1},
-                   {1.5708, 1, -1},
-                   {-3.1415, -1, -1},
-                   {0.0, -1, 1},
-                   {0.0, -1, 1},
-                   {0.0, -1, -1}}};
-    current_waypoint_index_ = 0;
-    dt_ = 3.0; // Time to reach each waypoint (adjust as needed)
+void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+  geometry_msgs::msg::Pose pose = msg->pose.pose;
+  tf2::Quaternion q(pose.orientation.x, pose.orientation.y, pose.orientation.z,
+                    pose.orientation.w);
+  tf2::Matrix3x3 m(q);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw); // Get roll, pitch, and yaw
+  phi = yaw;                  // Update the global phi with the yaw value
+}
 
-    motion_timer_ = this->create_wall_timer(
-        std::chrono::duration<double>(dt_), [this]() { moveToNextWaypoint(); });
-  }
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("absolute_motion");
+  auto pub = node->create_publisher<std_msgs::msg::Float32MultiArray>(
+      "wheel_speed", 10);
+  auto position_sub = node->create_subscription<nav_msgs::msg::Odometry>(
+      "/odometry/filtered", 10, odomCallback);
 
-private:
-  void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
-    current_position_.x = msg->pose.pose.position.x;
-    current_position_.y = msg->pose.pose.position.y;
+  rclcpp::sleep_for(std::chrono::seconds(1));
 
-    tf2::Quaternion q(
-        msg->pose.pose.orientation.x, msg->pose.pose.orientation.y,
-        msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
-    tf2::Matrix3x3 m(q);
-    m.getRPY(roll_, pitch_, yaw_);
-  }
+  auto velocity2twist = [](double dphi, double dx, double dy, double phi) {
+    double wz = dphi;
+    double vx = dx * cos(phi) + dy * sin(phi);
+    double vy = -dx * sin(phi) + dy * cos(phi);
+    return std::make_tuple(wz, vx, vy);
+  };
 
-  void moveToNextWaypoint() {
-    if (current_waypoint_index_ < waypoints_.size()) {
-      auto &waypoint = waypoints_[current_waypoint_index_];
-      RCLCPP_INFO(this->get_logger(),
-                  "Moving to waypoint %zu: dx=%f, dy=%f, dphi=%f",
-                  current_waypoint_index_, waypoint[1], waypoint[2], waypoint[0]);
-
-      double dphi = waypoint[0];
-      double dx = waypoint[1];
-      double dy = waypoint[2];
-
-      auto [wz, vx, vy] = displacement2velocity(dphi, dx, dy);
-
-      moveRobot(wz, vx, vy);
-
-      current_waypoint_index_++;
-    } else {
-      stopRobot();
-      RCLCPP_INFO(this->get_logger(), "Completed all waypoints.");
-    }
-  }
-
-  void moveRobot(double wz, double vx, double vy) {
-    std_msgs::msg::Float32MultiArray wheel_speed_msg;
-    auto [twist_wz, twist_vx, twist_vy] = velocity2twist(wz, vx, vy);
-    auto wheelSpeeds = twist2wheels(twist_wz, twist_vx, twist_vy);
-    wheel_speed_msg.data = {static_cast<float>(wheelSpeeds[0]),
-                            static_cast<float>(wheelSpeeds[1]),
-                            static_cast<float>(wheelSpeeds[2]),
-                            static_cast<float>(wheelSpeeds[3])};
-    wheel_speed_publisher_->publish(wheel_speed_msg);
-  }
-
-  std::array<double, 3> displacement2velocity(double dphi, double dx,
-                                              double dy) {
-    return {dphi / dt_, dx / dt_, dy / dt_};
-  }
-
-  std::array<double, 3> velocity2twist(double wz, double vx, double vy) {
-    double transformed_vx = std::cos(yaw_) * vx - std::sin(yaw_) * vy;
-    double transformed_vy = std::sin(yaw_) * vx + std::cos(yaw_) * vy;
-    return {wz, transformed_vx, transformed_vy};
-  }
-
-  std::array<double, 4> twist2wheels(double wz, double vx, double vy) {
+  auto twist2wheels = [](double wz, double vx, double vy) {
     double r = 0.05;        // radius in meters
     double l = 0.17 / 2;    // half of the wheelbase in meters
     double w = 0.26969 / 2; // half of the track width in meters
-    return {(-l - w) / r * wz + vx / r - vy / r,
-            (l + w) / r * wz + vx / r + vy / r,
-            (l + w) / r * wz + vx / r - vy / r,
-            (-l - w) / r * wz + vx / r + vy / r};
+
+    double u[4] = {0.0, 0.0, 0.0, 0.0};
+
+    u[0] = (-wz * (l + w) + vx - vy) / r;
+    u[1] = (wz * (l + w) + vx + vy) / r;
+    u[2] = (wz * (l + w) + vx - vy) / r;
+    u[3] = (-wz * (l + w) + vx + vy) / r;
+
+    return std::array<double, 4>{u[0], u[1], u[2], u[3]};
+  };
+
+  std::vector<std::tuple<double, double, double>> motions = {
+      {0.0, 1.0, -1.0},         {0.0, 1.0, 1.0},          {0.0, 1.0, 1.0},
+      {1.570796326, 1.0, -1.0}, {3.14159265, -1.0, -1.0}, {0.0, -1.0, 1.0},
+      {0.0, -1.0, 1.0},         {0.0, -1.0, -1.0},
+  };
+
+  for (const auto &motion : motions) {
+    double dphi = std::get<0>(motion) / 7; // Get dphi from the tuple
+    double dx = std::get<1>(motion) / 7;   // Get dx from the tuple
+    double dy = std::get<2>(motion) / 7;   // Get dy from the tuple
+
+    int iterations = 700;
+    for (int i = 0; i < iterations; i++) {
+      double wz, vx, vy;
+      std::tie(wz, vx, vy) = velocity2twist(dphi, dx, dy, phi);
+      rclcpp::spin_some(node);
+      std::array<double, 4> u = twist2wheels(wz, vx, vy);
+      auto msg = std::make_shared<std_msgs::msg::Float32MultiArray>();
+      msg->data.assign(u.begin(), u.end()); // Convert std::array to std::vector
+      pub->publish(*msg);
+      rclcpp::sleep_for(std::chrono::milliseconds(10));
+    }
   }
 
-  void stopRobot() {
-    std_msgs::msg::Float32MultiArray stop_msg;
-    stop_msg.data = {0.0f, 0.0f, 0.0f, 0.0f};
-    wheel_speed_publisher_->publish(stop_msg);
-  }
-
-  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscriber_;
-  rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr
-      wheel_speed_publisher_;
-  std::vector<std::array<double, 3>> waypoints_;
-  size_t current_waypoint_index_;
-  struct {
-    double x = 0.0;
-    double y = 0.0;
-  } current_position_;
-  double yaw_ = 0.0, roll_ = 0.0, pitch_ = 0.0;
-  double dt_;
-  rclcpp::TimerBase::SharedPtr motion_timer_;
-};
-
-int main(int argc, char **argv) {
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<EightTrajectoryNode>();
+  std::array<double, 4> stop = {0, 0, 0, 0};
+  auto stop_msg = std::make_shared<std_msgs::msg::Float32MultiArray>();
+  stop_msg->data.assign(stop.begin(),
+                        stop.end()); // Convert std::array to std::vector
+  pub->publish(*stop_msg);
   rclcpp::spin(node);
   rclcpp::shutdown();
+
   return 0;
 }
